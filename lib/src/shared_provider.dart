e76d07809a3a2a395f6261e445b8121686185dff
Magomed Dadagov <grozniy.info@gmail.com>
2024-08-19 18:27:07 +0300

    add startListening

--- lib/src/shared_provider.dart
+++ lib/src/shared_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
 
 import 'shared_instance.dart';
 
@@ -10,7 +11,7 @@ import 'shared_instance.dart';
 /// The value can be optionally disposed using [dispose] callback.
 /// This callback that will be called when the last [Provider]
 /// using the shared instance is unmounted from the widget tree.
-class SharedProvider<T> extends Provider<T> {
+class SharedProvider<T> extends SingleChildStatelessWidget {
   SharedProvider({
     required Create<T> acquire,
     required String instanceKey,
@@ -19,23 +20,46 @@ class SharedProvider<T> extends Provider<T> {
     Widget? child,
     TransitionBuilder? builder,
     bool? lazy,
-  }) : super(
-          key: key,
-          child: child,
-          builder: builder,
-          lazy: lazy,
-          create: (context) {
-            return SharedInstance.acquire(
-              createValue: () => acquire(context),
-              acquirer: context,
-              instanceKey: instanceKey,
-            ).value;
-          },
-          dispose: (context, value) {
-            SharedInstance.releaseIfAcquired(instanceKey, context);
-            if (!SharedInstance.hasAcquirer(instanceKey)) {
-              dispose?.call(context, value);
-            }
-          },
-        );
+    StartListening<T>? startListening,
+  })  : _startListening = startListening,
+        _acquire = acquire,
+        _instanceKey = instanceKey,
+        _dispose = dispose,
+        _lazy = lazy,
+        _builder = builder,
+        super(key: key, child: child);
+
+  final TransitionBuilder? _builder;
+  final bool? _lazy;
+  final Dispose<T>? _dispose;
+  final String _instanceKey;
+  final Create<T> _acquire;
+  final StartListening<T>? _startListening;
+
+  @override
+  Widget buildWithChild(BuildContext context, Widget? child) {
+    assert(
+      child != null,
+      '$runtimeType used outside of MultiBlocProvider must specify a child',
+    );
+    return InheritedProvider<T>(
+      create: (context) {
+        return SharedInstance.acquire(
+          createValue: () => _acquire(context),
+          acquirer: context,
+          instanceKey: _instanceKey,
+        ).value;
+      },
+      dispose: (context, value) {
+        SharedInstance.releaseIfAcquired(_instanceKey, context);
+        if (!SharedInstance.hasAcquirer(_instanceKey)) {
+          _dispose?.call(context, value);
+        }
+      },
+      startListening: _startListening,
+      lazy: _lazy,
+      builder: _builder,
+      child: child,
+    );
+  }
 }
